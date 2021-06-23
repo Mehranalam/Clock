eab13133653e0cdf6845f32ea21749a1fe937596
AmirHossein Mohammadi <55284339+BlackIQ@users.noreply.github.com>
2021-06-23 15:55:28 +0430

    New Code, Better, And this time it is working

--- Clock.ino
+++ Clock.ino
@@ -1,142 +1,42 @@
-#include <Wire.h>                   // for I2C communication
-#include <LiquidCrystal_I2C.h>      // for LCD
-#include <RTClib.h>                 // for RTC
+// Include Libs
+#include <LiquidCrystal_I2C.h> // LCD   16x2        Lib
+#include <DS3231.h>            // Real  Time Clock Lib
+#include <Wire.h>              // i2c    Protocol    Lib
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // create LCD with I2C address 0x27, 16 characters per line, 2 lines
-RTC_DS3231 rtc;                     // create rtc for the DS3231 RTC module, address is fixed at 0x68
+// Modules Setup
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);
+// Define Real Time Clock module
+DS3231 rtc(SDA , SCL);
 
-/*
-   function to update RTC time using user input
-*/
-void updateRTC()
-{
-  
-  lcd.clear();  // clear LCD display
-  lcd.setCursor(0, 0);
-  lcd.print("Edit Mode...");
+// Setup Void
+void setup() {
+  // Begin Modules
+  rtc.begin();  // RTC
+  lcd.init(     // LCD
 
-  // ask user to enter new date and time
-  const char txt[6][15] = { "year [4-digit]", "month [1~12]", "day [1~31]",
-                            "hours [0~23]", "minutes [0~59]", "seconds [0~59]"};
-  String str = "";
-  long newDate[6];
+  // Turn the backlight on
+  lcd.backlight();
 
-  while (Serial.available()) {
-    Serial.read();  // clear serial buffer
-  }
-
-  for (int i = 0; i < 6; i++) {
-
-    Serial.print("Enter ");
-    Serial.print(txt[i]);
-    Serial.print(": ");
-
-    while (!Serial.available()) {
-      ; // wait for user input
-    }
-
-    str = Serial.readString();  // read user input
-    newDate[i] = str.toInt();   // convert user input to number and save to array
-
-    Serial.println(newDate[i]); // show user input
-  }
-
-  // update RTC
-  rtc.adjust(DateTime(newDate[0], newDate[1], newDate[2], newDate[3], newDate[4], newDate[5]));
-  Serial.println("RTC Updated!");
+  // RTC Setup
+  rtc.setDOW(FRIDAY);         // Set the day  to like  : SUNDAY
+  rtc.setTime(15, 50, 0);     // Set the time to like : 12:00:00 (24hr format)
+  rtc.setDate(11, 9, 2020);   // Set the date to like : January 1st, 2014
 }
 
-/*
-   function to update LCD text
-*/
-void updateLCD()
-{
+// Main Loop
+void loop() {
+  // Define values
+  String tim = rtc.getTimeStr(); // Time
+  String dat = rtc.getDateStr(); // Date
 
-  /*
-     create array to convert digit days to words:
+  // Print Time
+  lcd.setCursor(0, 0); // Set spot
+  lcd.print("Time: ");
+  lcd.print(tim);      // Print Time
 
-     0 = Sunday    |   4 = Thursday
-     1 = Monday    |   5 = Friday
-     2 = Tuesday   |   6 = Saturday
-     3 = Wednesday |
-  */
-  const char dayInWords[7][4] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-
-  /*
-     create array to convert digit months to words:
-
-     0 = [no use]  |
-     1 = January   |   6 = June
-     2 = February  |   7 = July
-     3 = March     |   8 = August
-     4 = April     |   9 = September
-     5 = May       |   10 = October
-     6 = June      |   11 = November
-     7 = July      |   12 = December
-  */
-  const char monthInWords[13][4] = {" ", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", 
-                                         "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-
-  // get time and date from RTC and save in variables
-  DateTime rtcTime = rtc.now();
-
-  int ss = rtcTime.second();
-  int mm = rtcTime.minute();
-  int hh = rtcTime.twelveHour();
-  int DD = rtcTime.dayOfTheWeek();
-  int dd = rtcTime.day();
-  int MM = rtcTime.month();
-  int yyyy = rtcTime.year();
-
-  // move LCD cursor to upper-left position
-  lcd.setCursor(0, 0);
-
-  // print date in dd-MMM-yyyy format and day of week
-  if (dd < 10) lcd.print("0");  // add preceeding '0' if number is less than 10
-  lcd.print(dd);
-  lcd.print("-");
-  lcd.print(monthInWords[MM]);
-  lcd.print("-");
-  lcd.print(yyyy);
-
-  lcd.print("  ");
-  lcd.print(dayInWords[DD]);
-
-  // move LCD cursor to lower-left position
-  lcd.setCursor(0, 1);
-
-  // print time in 12H format
-  if (hh < 10) lcd.print("0");
-  lcd.print(hh);
-  lcd.print(':');
-
-  if (mm < 10) lcd.print("0");
-  lcd.print(mm);
-  lcd.print(':');
-
-  if (ss < 10) lcd.print("0");
-  lcd.print(ss);
-
-  if (rtcTime.isPM()) lcd.print(" PM"); // print AM/PM indication
-  else lcd.print(" AM");
-}
-
-void setup()
-{
-  Serial.begin(9600); // initialize serial
-
-  lcd.init();       // initialize lcd
-  lcd.backlight();  // switch-on lcd backlight
-
-  rtc.begin();       // initialize rtc
-}
-
-void loop()
-{
-  updateLCD();  // update LCD text
-
-  if (Serial.available()) {
-    char input = Serial.read();
-    if (input == 'u') updateRTC();  // update RTC time
-  }
+  // Print Date
+  lcd.setCursor(0, 1); // Set spot
+  lcd.print("Date: ");
+  lcd.print(dat);      // Print Date
 }
